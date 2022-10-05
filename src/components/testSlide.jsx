f92a1c81a390f5fd31052a783211c8b3482fe51f
ahcoo <mynameisjins@gmail.com>
2022-10-05 18:42:12 +0900

    create react

---
+++ src/components/testSlide.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const testSlide = () => {
+  return <div></div>;
+};
+
+export default testSlide;
---
+++ src/components/testVideo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const testVideo = () => {
+  return <div></div>;
+};
+
+export default testVideo;
