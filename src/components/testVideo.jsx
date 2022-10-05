f079b5d88b250b37947b9374f95cfe4454f51c38
BaeSangUn <zyxtkddns@naver.com>
2022-10-06 01:17:49 +0900

    동영상 나옴

--- src/components/testVideo.jsx
+++ src/components/testVideo.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const testVideo = () => {
-  return <div></div>;
+const TestVideo = () => {
+  return (
+    <div className="billboard-row">
+      <div className="billboard-motion">
+        <div className="nfp"></div>
+        <div className="motion-background-components">
+          <div
+            className="hero-image-wrapper"
+            style={{ width: "100%", height: "1000px" }}
+          >
+            <iframe
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/4OSFPrOL1LQ?autoplay=1&version=3&loop=1&playlist=4OSFPrOL1LQ&rel=0"
+              title=" video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            <div className="trailer-vignette"></div>
+            <div className="hero-vigenette"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default testVideo;
+export default TestVideo;
