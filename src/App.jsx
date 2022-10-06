efb5680f7bd76c31866591c5e9154f06a829fbfc
ahcoo <mynameisjins@gmail.com>
2022-10-06 23:47:28 +0900

    building slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import TestSlide from "./components/TestSlide";
 import TestVideo from "./components/TestVideo";
 function App() {
   return (
     <div className="App">
-      <TestVideo />
+      <Main />
     </div>
   );
 }
