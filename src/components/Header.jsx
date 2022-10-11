40da00fdf9610488d70b9ba69cd1dcd1f5bbd338
BaeSangUn <zyxtkddns@naver.com>
2022-10-11 18:47:02 +0900

    비디오 텍스트 수정 및 헤더 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      {" "}
+      <div className="header-top">
+        <div className="nav-box">
+          <div className="main-logo">
+            <ul>
+              <li>로고 사진</li>
+            </ul>
+          </div>
+          <div className="nav-main">
+            <ul>
+              <li>
+                <a href="/">홈</a>{" "}
+              </li>
+              <li>
+                <a href="/">시리즈</a>
+              </li>
+              <li>
+                <a href="/">영화</a>
+              </li>
+              <li>
+                <a href="/">NEW! 요즘 대세 컨텐츠</a>
+              </li>
+              <li>
+                <a href="/">내가 침한 콘텐츠</a>
+              </li>
+              <li>
+                <a href="/">언어 별로 찾아보기</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="/">검색 아이콘</a>
+              </li>
+              <li>
+                <a href="/">키즈</a>
+              </li>
+              <li>
+                <a href="/">알림 아이콘</a>
+              </li>
+              <li>
+                <a href="/">로그인 프로필</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
