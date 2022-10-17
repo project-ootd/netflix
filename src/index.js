136f68bcdb61e3af0328fe1c1f06708bd6ebae9c
nimi2838 <bbongsky11@naver.com>
2022-10-17 18:43:47 +0900

    로그인 회원가입페이지 프론트

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import First from "./components/First";
 import Login from "./components/Login";
+import RegForm from "./components/RegForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,8 +15,7 @@ root.render(
       <Route path="/" element={<App />}></Route>
       {/* <Route path="/first" element={<First />}></Route> */}
       <Route path="/login" element={<Login />}></Route>
-      {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-      {/* <Route path="*" element={<NotFound />}></Route> */}
+      <Route path="/regform" element={<RegForm />}></Route>
     </Routes>
   </BrowserRouter>
   // </React.StrictMode>
