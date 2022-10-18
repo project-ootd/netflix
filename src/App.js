8a7a56f210d7731924d26d610fe35a7e87237c60
ahcoo <mynameisjins@gmail.com>
2022-10-18 18:45:08 +0900

    한국 드라마 재수정 중

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
       {/* <Login /> */}
       {/* <Slides /> */}
-      {/* <KoreanDrama /> */}
+      <KoreanDrama />
       {/* <TestVideo /> */}
       {/* <RankingSlide /> */}
 
--- src/slides/KoreanDrama.jsx
+++ src/slides/KoreanDrama.jsx
@@ -39,17 +39,16 @@ const KoreanDrama = () => {
   };
   return (
     <div className="korean-drama-slide">
-      <div className="korean-drama-text flex flex_jc_start">
-        <a href="#">
-          한국 드라마
-          <div className="korean-drama-text2">모두 보기</div>
-          <img
-            className="korean-drama-arrow"
-            src="https://img.icons8.com/metro/16/54b9c5/forward.png"
-          />
-        </a>
+      <div className="korean-drama-title">
+        <div className="korean-drama-text flex flex_jc_start">
+          <a href="#">한국 드라마</a>
+        </div>
+        <div className="korean-drama-text2">모두 보기</div>
+        <img
+          className="korean-drama-arrow"
+          src="https://img.icons8.com/metro/16/54b9c5/forward.png"
+        />
       </div>
-
       <Slider {...settings}>
         {/* 1번째 */}
         <div className="korean-drama kd-1">
