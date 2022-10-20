d65382680e63d7daaf610213213cc9ba54ab0d18
ahcoo <mynameisjins@gmail.com>
2022-10-21 00:25:07 +0900

    한국드라마 슬라이드 완성

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       {/* <RegForm /> */}
       {/* <Login /> */}
       {/* <Slides /> */}
-      {/* <KoreanDrama /> */}
+      <KoreanDrama />
       {/* <TestVideo /> */}
 
       {/* <KoreanDrama /> */}
